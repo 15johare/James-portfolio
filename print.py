b0c468a9370178650cc1e0c85b4718bcd36a9d6e
15johare <15johare@stjosephsrush.ie>
2017-05-18 15:22:40 +0100

    Create print.py

---
+++ print.py
@@ -0,0 +1,3 @@
+print("this is how to write a print statement")
+  #all you have to do is write the word print and you a bracekt and bunny ears write what you want to say and then bunny ears bracket again
+  #there is an example up above
