c865aa7d9a2c669c9f0a7a9b4cd248e511d160f5
15johare <15johare@stjosephsrush.ie>
2017-05-23 10:21:18 +0100

    Create if elif and else code.py

---
+++ if elif and else code.py
@@ -0,0 +1,31 @@
+print("this is an if elif and else code")
+
+day=input("choose a day")
+
+
+if day=="monday":
+ print("i do not have football today")
+
+elif day=="tuesday":
+ print("i have football today")
+
+elif day=="wednesday":
+ print("i do not have football today")
+
+elif day=="thursday":
+ print("i do not have football today")
+
+elif day=="friday":
+ print("i do not have football today")
+
+elif day=="saturday":
+ print("i do not have football today")
+
+elif day=="sunday":
+ print("i do not have football today")
+
+else:
+  print("this is not a day of the week")
+
+     #and this is how to do an if elif and else code
+
